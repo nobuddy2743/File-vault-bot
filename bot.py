412b607f4dd73d5e952e455bb215b6ddac560812
gitdrive5 <121691578+gitdrive5@users.noreply.github.com>
2023-01-29 17:26:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-# (c) @TeleRoidGroup || @PredatorHackerzZ
 
 import os
 import asyncio
@@ -363,7 +362,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                 user = await bot.get_chat_member(channel_chat_id, cmd.message.chat.id)
                 if user.status == "kicked":
                     await cmd.message.edit(
-                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/TeleRoid14).",
+                        text="Sorry Sir, You are Banned to use me.",
                         disable_web_page_preview=True
                     )
                     return
