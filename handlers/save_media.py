67d38c793a5860fad4e21c731c46c1d42649a004
gitdrive5 <121691578+gitdrive5@users.noreply.github.com>
2023-05-24 14:29:18 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -49,7 +49,7 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
                 InlineKeyboardButton("Delete Batch", callback_data="closeMessage")
             ]])
         )
-        share_link = f"https://telegram.dog/{Config.BOT_USERNAME}?start=tgnvs_{str_to_b64(str(SaveMessage.id))}"
+        share_link = f"https://animetgstorebot.blogspot.com/?fid=tgnvs_{str_to_b64(str(SaveMessage.id))}"
         await editable.edit(
             f"**Batch Files Stored in my Database!**\n\nHere is the Permanent Link of your files: {share_link} \n\n"
             f"Just Click the link to get your files!",
@@ -89,7 +89,7 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         await forwarded_msg.reply_text(
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
             disable_web_page_preview=True)
-        share_link = f"https://telegram.dog/{Config.BOT_USERNAME}?start=tgnvs_{str_to_b64(file_er_id)}"
+        share_link = f"https://animetgstorebot.blogspot.com/?fid=tgnvs_{str_to_b64(file_er_id)}"
         await editable.edit(
             "**Your File Stored in my Database!**\n\n"
             f"Here is the Permanent Link of your file: {share_link} \n\n"
