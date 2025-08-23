e8286d65c9c464de417a928ca12964f54aad3484
HIREN PATEL <111114081+BUDDY1609@users.noreply.github.com>
2025-08-23 15:39:28 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -12,7 +12,7 @@ async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
             f"**Here is Sharable Link of this File:**\n"
-            f"https://animetgstorebot.blogspot.com/p/home.html?fid=tgnvs_{str_to_b64(str(file_id))}\n\n"
+            f"https://qthicsbot.blogspot.com/tg/{str_to_b64(str(file_id))}\n\n"
             f"__To Retrive the Stored File, just open the link!__",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
